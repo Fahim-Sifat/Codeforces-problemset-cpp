0a3a5d341a5af77cfd03a6c72da784e016ad753c
codewithfahim2025 <codewithfahim2025@gmail.com>
2026-04-16 21:40:30 +0600

    _236A added

---
+++ Codeforces/_112A-petya-and-strings.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+
+    string string1,string2;
+
+    cin>>string1>>string2;
+    for(int i=0;i<string1.size();i++){
+        string1[i]=tolower(string1[i]);
+    }
+
+    for(int i=0;i<string2.size();i++){
+        string2[i]=tolower(string2[i]);
+    }
+
+    if(string1==string2){
+        cout<<0<<endl;
+    }
+    else if(string1>string2){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<-1<<endl;
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*A. Petya and Strings
+
+Little Petya loves presents. His mum bought him two strings of the same size for his birthday. The strings consist of uppercase and lowercase Latin letters. Now Petya wants to compare those two strings lexicographically. The letters' case does not matter, that is an uppercase letter is considered equivalent to the corresponding lowercase letter. Help Petya perform the comparison.
+
+Input
+Each of the first two lines contains a bought string. The strings' lengths range from 1 to 100 inclusive. It is guaranteed that the strings are of the same length and also consist of uppercase and lowercase Latin letters.
+
+Output
+If the first string is less than the second one, print "-1". If the second string is less than the first one, print "1". If the strings are equal, print "0". Note that the letters' case is not taken into consideration when the strings are compared.
+
+Examples
+InputCopy
+aaaa
+aaaA
+OutputCopy
+0
+InputCopy
+abs
+Abz
+OutputCopy
+-1
+InputCopy
+abcdefg
+AbCdEfF
+OutputCopy
+1
+Note
+If you want more formal information about the lexicographical order (also known as the "dictionary order" or "alphabetical order"), you can visit the following site:
+
+http://en.wikipedia.org/wiki/Lexicographical_order
+*/
